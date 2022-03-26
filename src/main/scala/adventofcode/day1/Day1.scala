21dd7376906b7a789a93d3f5fa0e5733a6b977cf
Kamil Rafałko <kamil.rafalko@softwaremill.com>
2022-03-26 10:41:44 +0100

    Initial commit

---
+++ src/main/scala/adventofcode/day1/Day1.scala
@@ -0,0 +1,16 @@
+package adventofcode.day1
+
+import scala.io.Source
+
+class Day1 {
+//  def compute(stream: InputStream): Unit =  {
+//
+//  }
+}
+
+object Day1 {
+  def main(args: Array[String]): Unit = {
+    val source = Source.fromResource("day1/day1.txt")
+    println(source.mkString)
+  }
+}
---
+++ src/test/scala/com/adventofcode/day1/Day1Spec.scala
@@ -0,0 +1,10 @@
+package com.adventofcode.day1
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Day1Spec extends FlatSpec with Matchers {
+
+  it should "count frequency" in {
+
+  }
+}
